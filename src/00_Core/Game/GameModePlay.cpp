9e3857bd733f68496bf072e4635a2a2e737c6d8b
Axel <warukho@gmail.com>
2026-06-05 01:16:22 +0200

    decomp: match 6 fns in GameModePlay.cpp (eur)

--- src/00_Core/Game/GameModePlay.cpp
+++ src/00_Core/Game/GameModePlay.cpp
@@ -1,7 +1,24 @@
 #include "Game/GameModePlay.hpp"
 
-GameModePlay::GameModePlay(unk32 param1) :
-    GameMode(param1) {}
-GameModePlay::~GameModePlay() {}
-void GameModePlay::vfunc_1c() {}
-void GameModePlay::vfunc_20() {}
+#include "DTCM/UnkStruct_027e0c54.hpp"
+
+extern "C" void func_02036074(UnkStruct_027e0c54 *param1);
+extern "C" void func_0201823c(void *param1);
+extern "C" void func_0201825c();
+
+extern GameModePlay *data_027e0d40;
+extern unk32 data_027e0934;
+
+THUMB GameModePlay::GameModePlay(unk32 param1) :
+    GameMode(param1) {
+    data_027e0d40 = this;
+    func_0201823c(&data_027e0934);
+}
+THUMB GameModePlay::~GameModePlay() {
+    func_0201825c();
+    data_027e0d40 = NULL;
+}
+THUMB void GameModePlay::vfunc_1c() {
+    func_02036074(&data_027e0c54);
+}
+THUMB void GameModePlay::vfunc_20() {}
